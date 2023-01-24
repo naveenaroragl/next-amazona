235e8a5d5c1b729eb40ec07b62e08b0b16fe18d8
Naveen Arora <naveenarora@deloitte.co.uk>
2023-01-24 17:42:14 +0000

    product listing and PDP

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Layout from "@/components/Layout";
+import ProductItem from "@/components/ProductItem";
+import data from "@/utils/data";
 
 export default function Home() {
   return (
     <Layout title="Homepage">
-      <h1 className="text-3xl font-bold">NEXT tailwind amazona</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem
+            product={product}
+            key={product.slug}
+            // addToCartHandler={addToCartHandler}
+          ></ProductItem>
+        ))}
+      </div>
     </Layout>
   );
 }
